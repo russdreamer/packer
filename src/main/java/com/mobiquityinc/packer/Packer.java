a3a4e92d5f6c7bb9fd2069def76613f67ae08256
Igor K <russdreamer@gmail.com>
2019-08-19 03:27:15 +0300

    implement validation and parsing cases and item by regex

--- src/main/java/com/mobiquityinc/packer/Packer.java
+++ src/main/java/com/mobiquityinc/packer/Packer.java
@@ -1,9 +1,17 @@
 package com.mobiquityinc.packer;
 
 import com.mobiquityinc.exception.APIException;
+import com.mobiquityinc.pojo.Case;
+import com.mobiquityinc.pojo.Item;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Packer {
@@ -21,12 +29,52 @@ public class Packer {
         return cases.stream().map(Packer::getSuitableItems).collect(Collectors.joining(System.lineSeparator()));
     }
 
-    private static List<String> readLines(String filePath) {
-        return null;
+    private static List<String> readLines(String filePath) throws APIException {
+        if (filePath == null) {
+            throw new APIException("Invalid file path");
+        }
+
+        File file = new File(filePath);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            return reader.lines().collect(Collectors.toList());
+
+        } catch (FileNotFoundException e) {
+            throw new APIException("Invalid file path");
+        }
     }
 
-    private static Case parseCase(String line) {
-        return null;
+    private static Case parseCase(String line) throws APIException {
+        Pattern pattern = Pattern.compile("^\\d+\\s*:(\\s*\\([^()]*\\)\\s*)*$");
+        if (!pattern.matcher(line).matches()) {
+            throw new APIException("The file's line contains incorrect parameters");
+        }
+
+        String[] parts = line.split(":");
+        int weightLimit = Integer.valueOf(parts[0].trim());
+        List<Item> items = parseItems(parts[1]);
+        return new Case(weightLimit, items);
+    }
+
+    private static List<Item> parseItems(String line) throws APIException {
+        String groupRegex = "(?:\\((\\d+),(\\d+(?:[.]\\d+)*),€(\\d+)\\))";
+        String validateRegex = "(\\s" + groupRegex + "\\s*)*";
+
+        Matcher validateMatcher = Pattern.compile(validateRegex).matcher(line);
+        if (!validateMatcher.matches()){
+            throw new APIException("The file's line contains incorrect parameters");
+        }
+
+        Matcher groupMatcher = Pattern.compile(groupRegex).matcher(line);
+        List<Item> items = new LinkedList<>();
+        while (groupMatcher.find()) {
+            items.add(new Item(
+                    Integer.valueOf(groupMatcher.group(1)),
+                    Float.valueOf(groupMatcher.group(2)),
+                    Integer.valueOf(groupMatcher.group(3))
+            ));
+        }
+        return items;
     }
 
     private static String getSuitableItems(Case caseInst) {
