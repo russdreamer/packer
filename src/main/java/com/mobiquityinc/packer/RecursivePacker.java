70286d6cd7d37a5bfdfaf76e50ab92a46ce1dc8c
Igor K <russdreamer@gmail.com>
2019-08-19 22:04:15 +0300

    implement recursive method to find optimal items

--- src/main/java/com/mobiquityinc/packer/RecursivePacker.java
+++ src/main/java/com/mobiquityinc/packer/RecursivePacker.java
@@ -3,11 +3,75 @@ package com.mobiquityinc.packer;
 import com.mobiquityinc.pojo.Case;
 import com.mobiquityinc.pojo.Item;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
-public class RecursivePacker {
+class RecursivePacker {
 
-    public static List<Item> getPackedItems(Case caseInst) {
-        return null;
+    static List<Item> getPackedItems(Case caseInst) {
+        List<Item> items = caseInst.getItems();
+        Package pack = findRecursively(caseInst.getWeightLimit(), items, 0, -1);
+        return pack.items;
+    }
+
+    private static Package findRecursively(int weightLimit, List<Item> items, float totalWeight, int index) {
+        Item curItem = index < 0 ? null: items.get(index);
+        int curItCost = curItem == null ? 0 : curItem.getCost();
+        float curItWeight = curItem == null ? 0 : curItem.getWeight();
+        Package bestChildPack = null;
+
+        for (int i = index + 1; i < items.size(); i++) {
+            float nextWeight = totalWeight + items.get(i).getWeight();
+
+            if (nextWeight <= weightLimit) {
+                Package childPack = findRecursively(weightLimit, items, nextWeight, i);
+
+                if (bestChildPack == null || childPack.cost > bestChildPack.cost) {
+                    bestChildPack = childPack;
+                } else {
+                    if (childPack.cost == bestChildPack.cost) {
+                        if (childPack.weight < bestChildPack.weight) {
+                            bestChildPack = childPack;
+                        }
+                    }
+                }
+            }
+        }
+
+        return getCurrentPackage(bestChildPack, curItWeight, curItCost, curItem);
+    }
+
+    private static Package getCurrentPackage(Package bestChildPack, float curItWeight, int curItCost, Item curItem) {
+        List<Item> items;
+
+        if (bestChildPack != null) {
+            curItWeight += bestChildPack.weight;
+            curItCost += bestChildPack.cost;
+            items = bestChildPack.items;
+            if (curItem != null) {
+                items.add(0, curItem);
+            }
+        }
+        else {
+            if (curItem != null) {
+                items = new LinkedList<>();
+                items.add(curItem);
+            }
+            else items = Collections.emptyList();
+        }
+        return new Package(curItWeight, curItCost, items);
+    }
+
+    private static class Package {
+        private float weight;
+        private int cost;
+        private List<Item> items;
+
+        private Package(float weight, int cost, List<Item> items) {
+            this.weight = weight;
+            this.cost = cost;
+            this.items = items;
+        }
     }
 }
